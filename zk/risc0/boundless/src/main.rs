4ca9c1f2fb181bbf81fb9cf89ec57729a40485fb
Preston Ong <preston4896@me.com>
2025-02-14 11:25:40 +0800

    updated script

--- zk/risc0/boundless/src/main.rs
+++ zk/risc0/boundless/src/main.rs
@@ -1,3 +1,7 @@
+use anyhow::{Error, Result};
+
+use pccs_reader_rs::{find_missing_collaterals_from_quote, MissingCollateral};
+
 use boundless::attestation::IAttestation;
 use boundless_market::{
     alloy::{
@@ -9,7 +13,6 @@ use boundless_market::{
     input::InputBuilder,
     storage::{StorageProviderConfig, StorageProviderType},
 };
-// use methods::{DCAP_GUEST_ELF, DCAP_GUEST_ID};
 use risc0_zkvm::{
     compute_image_id, default_executor,
     sha::{Digest, Digestible},
@@ -22,39 +25,51 @@ pub const DCAP_GUEST_ELF: &[u8] = include_bytes!(
 );
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
-    let image_id = compute_image_id(DCAP_GUEST_ELF).unwrap();
+    let image_id = compute_image_id(DCAP_GUEST_ELF)?;
 
-    println!("ImageID: {}", &image_id.to_string());
+    tracing::info!("ImageID: {}", &image_id.to_string());
 
-    // TODO: implement methods to properly serialize quotes with its collateral to generate Guest program input...
+    let current_time = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)?
+        .as_secs();
 
-    // let current_time = std::time::SystemTime::now()
-    //     .duration_since(std::time::UNIX_EPOCH)
-    //     .unwrap()
-    //     .as_secs();
+    let quote = hex::decode(include_str!("../data/quote.hex"))?;
+    let ret = find_missing_collaterals_from_quote(&quote).await;
+    let serialized_collateral = if let MissingCollateral::None(collaterals) = ret {
+        collaterals
+    } else {
+        return Err(Error::msg(format!("{:?}", ret)));
+    };
 
-    // let quote = hex::decode(include_str!("../data/quote.hex")).unwrap();
+    let mut input: Vec<u8> = vec![];
+    let quote_len = quote.len() as u32;
+    let collaterals_len = serialized_collateral.len() as u32;
+    input.extend_from_slice(&current_time.to_le_bytes());
+    input.extend_from_slice(&quote_len.to_le_bytes());
+    input.extend_from_slice(&collaterals_len.to_le_bytes());
+    input.extend_from_slice(&quote);
+    input.extend_from_slice(&serialized_collateral);
 
-    let input = hex::decode(include_str!("../data/input.hex")).unwrap();
+    request_proof_then_verify(&input, &image_id).await?;
 
-    request_proof_then_verify(&input, &image_id).await;
+    Ok(())
 }
 
-async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
-    let rpc_url = std::env::var("SEPOLIA_URL").unwrap().parse().unwrap();
+async fn request_proof_then_verify(input: &[u8], image_id: &Digest) -> Result<()> {
+    let rpc_url = std::env::var("SEPOLIA_URL")?.parse()?;
     let boundless_market_address = address!("69c7943DA0D7e45D44Bd0cE7a2412DCdAe423788");
     let set_verifier_address = address!("Ef0A93B2310d52358F1eCA0C946aD7D25596e7dd");
-    let order_stream_url = Some("https://order-stream.beboundless.xyz".parse().unwrap());
+    let order_stream_url = Some("https://order-stream.beboundless.xyz".parse()?);
     let storage_config = Some(StorageProviderConfig {
         storage_provider: StorageProviderType::Pinata,
-        pinata_jwt: Some(std::env::var("PINATA_JWT").unwrap()),
-        pinata_api_url: Some(std::env::var("PINATA_API_URL").unwrap().parse().unwrap()),
-        ipfs_gateway_url: Some(std::env::var("IPFS_GATEWAY_URL").unwrap().parse().unwrap()),
+        pinata_jwt: Some(std::env::var("PINATA_JWT")?),
+        pinata_api_url: Some(std::env::var("PINATA_API_URL")?.parse()?),
+        ipfs_gateway_url: Some(std::env::var("IPFS_GATEWAY_URL")?.parse()?),
         s3_access_key: None,
         s3_secret_key: None,
         s3_url: None,
@@ -62,8 +77,8 @@ async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
         aws_region: None,
         file_path: None,
     });
-    let private_key_bytes = hex::decode(std::env::var("PRIVATE_KEY").unwrap()).unwrap();
-    let private_key = PrivateKeySigner::from_slice(private_key_bytes.as_slice()).unwrap();
+    let private_key_bytes = hex::decode(std::env::var("PRIVATE_KEY")?)?;
+    let private_key = PrivateKeySigner::from_slice(private_key_bytes.as_slice())?;
 
     let boundless_client = ClientBuilder::default()
         .with_rpc_url(rpc_url)
@@ -73,8 +88,7 @@ async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
         .with_storage_provider_config(storage_config.clone())
         .with_private_key(private_key)
         .build()
-        .await
-        .unwrap();
+        .await?;
 
     let image_url =
         "https://gateway.pinata.cloud/ipfs/QmTEucNxkAmv8UPMGmq34wgwBtLdiYEFPKiwa2AtzwMts1";
@@ -82,12 +96,11 @@ async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
 
     let input_builder = InputBuilder::new().write_slice(&input);
 
-    let guest_env = input_builder.clone().build_env().unwrap();
-    let guest_env_bytes = guest_env.encode().unwrap();
+    let guest_env = input_builder.clone().build_env()?;
+    let guest_env_bytes = guest_env.encode()?;
 
-    let session_info = default_executor()
-        .execute(guest_env.try_into().unwrap(), DCAP_GUEST_ELF)
-        .unwrap();
+    std::env::set_var("RISC0_INFO", "1");
+    let session_info = default_executor().execute(guest_env.try_into()?, DCAP_GUEST_ELF)?;
 
     let mcycle_count = session_info
         .segments
@@ -109,14 +122,13 @@ async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
         ))
         .with_offer(
             Offer::default()
-                .with_min_price_per_mcycle(parse_ether("0.001").unwrap(), mcycle_count)
-                .with_max_price_per_mcycle(parse_ether("0.002").unwrap(), mcycle_count)
+                .with_min_price_per_mcycle(parse_ether("0.001")?, mcycle_count)
+                .with_max_price_per_mcycle(parse_ether("0.002")?, mcycle_count)
                 .with_timeout(1000),
         )
-        .build()
-        .unwrap();
+        .build()?;
 
-    let (request_id, expires_at) = boundless_client.submit_request(&request).await.unwrap();
+    let (request_id, expires_at) = boundless_client.submit_request(&request).await?;
 
     println!(
         "Request submitted, id: {}, expires at: {}",
@@ -128,8 +140,7 @@ async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
 
     let (journal_bytes, seal) = boundless_client
         .wait_for_request_fulfillment(request_id, Duration::from_secs(5), expires_at)
-        .await
-        .unwrap();
+        .await?;
 
     let dcap_address = address!("E28ea4E574871CA6A4331d6692bd3DD602Fb4f76");
     let attestation_contract = IAttestation::new(dcap_address, boundless_client.provider().clone());
@@ -138,7 +149,7 @@ async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
         .verifyAndAttestWithZKProof(journal_bytes, 1, seal)
         .from(boundless_client.caller());
 
-    let pending_tx = attestation_call.send().await.unwrap();
+    let pending_tx = attestation_call.send().await?;
 
     let tx_hash = pending_tx
         .with_timeout(Some(TX_TIMEOUT))
@@ -147,4 +158,6 @@ async fn request_proof_then_verify(input: &[u8], image_id: &Digest) {
         .expect("failed to confirm tx");
 
     println!("Tx hash: {}", tx_hash.to_string());
+
+    Ok(())
 }
