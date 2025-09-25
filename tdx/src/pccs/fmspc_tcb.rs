fff054885c2786a55717c2b1085aff0feaed7b64
phrwlk <phrwlk7@gmail.com>
2025-09-25 21:13:01 +0300

    Update fmspc_tcb.rs

--- tdx/src/pccs/fmspc_tcb.rs
+++ tdx/src/pccs/fmspc_tcb.rs
@@ -42,7 +42,7 @@ pub async fn get_tcb_info(tcb_type: u8, fmspc: &str, version: u32) -> Result<Vec
 
     if tcb_info_str.len() == 0 || signature_bytes.len() == 0 {
         return Err(anyhow::Error::msg(format!(
-            "TCBInfo for FMSPC: {}; Version: {} is missing and must be upserted to on-chain pccs",
+            "TCBInfo for FMSPC: {}; Version: {} is missing and must be inserted to on-chain pccs",
             fmspc, version
         )));
     }
