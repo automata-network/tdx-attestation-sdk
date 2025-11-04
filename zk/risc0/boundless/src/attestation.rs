f3cf5e4606a1c15e48612c3fda3f36144fe30db6
Preston Ong <preston4896@me.com>
2025-11-04 14:55:38 +0800

    clean up

--- zk/risc0/boundless/src/attestation.rs
+++
@@ -1,8 +0,0 @@
-use boundless_market::alloy::sol;
-
-sol! {
-    #[sol(rpc)]
-    interface IAttestation {
-        function verifyAndAttestWithZKProof(bytes calldata journal, uint8 zkCoProcessor, bytes calldata seal) returns (bool success, bytes memory output);
-    }
-}
--- zk/risc0/host/src/boundless/mod.rs
+++ zk/risc0/host/src/boundless/mod.rs
@@ -1,5 +1,3 @@
-// Attestation module commented out for now due to name conflict with core crate
-// pub mod attestation;
-pub mod proving;
 
+pub mod proving;
 pub use proving::{request_proof, ProofRequestConfig, ProofType};
