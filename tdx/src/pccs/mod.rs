d346c05f364554980e7819e8ff7471ad03e4ad10
exh0115 <165619875+exh0115@users.noreply.github.com>
2025-12-16 18:55:37 +0800

    Add TCB Eval number

--- tdx/src/pccs/mod.rs
+++ tdx/src/pccs/mod.rs
@@ -6,6 +6,7 @@ pub mod pcs;
 pub const DEFAULT_RPC_URL: &str = "https://1rpc.io/ata/testnet";
 
 // PCCS addresses
+// TCB Eval Number = 18 (Standard until 2026-05-14)
 pub const ENCLAVE_ID_DAO_ADDRESS: &str = "6eE9602b90E8C451FfBCc8d5Dc9C8A3BF0A4fA56";
 pub const FMSPC_TCB_DAO_ADDRESS: &str = "62E8Cd513B12F248804123f7ed12A0601B79FBAc";
 pub const PCS_DAO_ADDRESS: &str = "B270cD8550DA117E3accec36A90c4b0b48daD342";
